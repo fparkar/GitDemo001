051b868fbcfda4e3f8b80fffa98846d6576e7fee
hardtask <hardtask@hs-MacBook-Pro.local>
2018-01-27 20:26:14 +0300

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by hardtask on 1/27/18.
+//  Copyright © 2018 hardtask. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by hardtask on 1/27/18.
+//  Copyright © 2018 hardtask. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
